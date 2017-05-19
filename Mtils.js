5730655aa6202c65ae408fbe73854cdb7e14879a
zhangrui <zhangrui@lymsh.com>
2017-05-19 16:35:40 +0800

    add method Mtils.validation.isCreditCode

--- Mtils.js
+++ Mtils.js
@@ -217,6 +217,49 @@ window.Mtils = {
 
 		/**
 		 * @author Rui.Zhang
+		 * @description 判断是否为统一社会信用代码
+		 * @param {String} str_data 待校验的数据
+		 * @returns {Boolean}, true:社会信用代码正确
+		 **/
+		isCreditCode : function (str_data) {
+			var patrn = /^[0-9A-Z]+$/;
+			//18位校验及大写校验
+			if ((str_data.length != 18) || (patrn.test(str_data) == false)) { 
+		　　　　　　return false;
+		　　　　 } else { 
+		　　　　var Ancode;//统一社会信用代码的每一个值
+		　　　　var Ancodevalue;//统一社会信用代码每一个值的权重 
+		　　　　var total = 0; 
+		　　　　var weightedfactors = [1, 3, 9, 27, 19, 26, 16, 17, 20, 29, 25, 13, 8, 24, 10, 30, 28];//加权因子 
+		　　　　var str = '0123456789ABCDEFGHJKLMNPQRTUWXY';
+		　　　　//不用I、O、S、V、Z 
+		　　　　for (var i = 0; i < str_data.length - 1; i++) 
+		　　　　{
+		　　　　Ancode = str_data.substring(i, i + 1); 
+		　　　　Ancodevalue = str.indexOf(Ancode); 
+		　　　　total = total + Ancodevalue * weightedfactors[i];
+		　　　　//权重与加权因子相乘之和 
+		　　　　}
+		　　　　var logiccheckcode = 31 - total % 31;
+		　　　　if (logiccheckcode == 31) {
+		　　　　　　logiccheckcode = 0;
+		　　　　}
+		　　　　var Str = "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,J,K,L,M,N,P,Q,R,T,U,W,X,Y";
+		　　　　var Array_Str = Str.split(',');
+		　　　　logiccheckcode = Array_Str[logiccheckcode];
+
+
+		　　　　var checkcode = str_data.substring(17, 18);
+		　　　　if (logiccheckcode != checkcode) { 
+		　　　　　　return false;
+		　　　　}
+				return true;
+			}
+		},
+
+
+		/**
+		 * @author Rui.Zhang
 		 * @description 判断是否为银行卡号
 		 * @param {String} str_cardNo 待校验的数据
 		 * @returns {Boolean}, true:是银行卡号
