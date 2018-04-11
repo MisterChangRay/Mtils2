7db4af7e35f9013ab268a6c68e7a11c4c33c9217
rui.zhang <zhangrui@ejie365.com>
2018-04-11 18:38:12 +0800

    publish on npm

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/*
+    BSD 3-Clause License https://github.com/MisterChangRay/Mtils2/blob/master/LICENSE
+    Author zhangrui
+*/
+var path = require('path');
+
+console.log("welcome mtils");
